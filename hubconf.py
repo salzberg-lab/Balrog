e1aba04277f96ab17eb504fb089205a20e8b0749
Markus Sommer <markusjsommer@gmail.com>
2020-08-11 14:54:40 -0400

    model update

--- hubconf.py
+++ hubconf.py
@@ -20,7 +20,7 @@ def geneTCN():
     n_classes = 1
     kernel_size = 8
     dropout = 0.05
-    hidden_units_per_layer = 32
+    hidden_units_per_layer = 25
     levels = 5
     channel_sizes = [hidden_units_per_layer] * levels
     
